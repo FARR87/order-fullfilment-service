8b9db0ab02f4bbc2590911be77c0e5107e6c14ea
Fabri Ramos <fabrizioramos@gmail.com>
2026-03-17 22:30:37 +0000

    cleanup

--- server/src/database/extra-setup.js
+++
@@ -1,8 +0,0 @@
-function applyExtraSetup(sequelize) {
-    const { Order, orderItem } = sequelize.models;
-
-    order.HasMany(orderItem);
-    orderItem.BelongsTo(order);
-}
-
-module.exports = { applyExtraSetup };
